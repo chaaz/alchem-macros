9e5a8d65f608dcfdebde17d1ca546c68af05bd96
Charlie Ozinga <ozchaz@gmail.com>
2021-02-19 17:00:07 -0700

    feat: add native_fn and native_tfn macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,127 @@
 extern crate proc_macro;
+
+use proc_macro2::Span;
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn, Lifetime, LifetimeDef, FnArg, Generics, GenericParam, ReturnType, Type};
+use syn::punctuated::Punctuated;
+use syn::token::{Lt, Gt};
 
 #[proc_macro_attribute]
 pub fn native_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
-  item
+  let func = parse_macro_input!(item as ItemFn);
+  let attrs = &func.attrs;
+  let vis = &func.vis;
+  let sig = &func.sig;
+  assert!(sig.asyncness.is_some());
+
+  let g0_lifetime = Lifetime::new("'r", Span::call_site());
+  let g0 = GenericParam::Lifetime(
+    LifetimeDef { attrs: Vec::new(), lifetime: g0_lifetime, colon_token: None, bounds: Punctuated::new() }
+  );
+  let mut generics = sig.generics.params.iter();
+  let g1 = generics.next();
+  let mut params = Punctuated::new();
+  params.insert(0, g0);
+  if let Some(g1) = g1 {
+    params.insert(1, g1.clone());
+  }
+  let new_generics = Generics {
+    lt_token: Some(Lt { spans: [Span::call_site()] }),
+    gt_token: Some(Gt { spans: [Span::call_site()] }),
+    params,
+    where_clause: None
+  };
+
+  let name = &sig.ident;
+  let mut inputs = sig.inputs.iter();
+  let arg1 = inputs.next().unwrap();
+  let arg2 = inputs.next().unwrap();
+  let arg3 = inputs.next().unwrap();
+  let (arg3_i, arg3_t) = match arg3 {
+    FnArg::Receiver(_) => panic!("Unexpected receiver argument."),
+    FnArg::Typed(t) => (&t.pat, &t.ty)
+  };
+  let arg3_t = match arg3_t.as_ref() {
+    Type::Reference(r) => {
+      assert!(r.mutability.is_some());
+      assert!(r.lifetime.is_none());
+      &r.elem
+    },
+    _ => panic!("Expected type reference in arg3")
+  };
+  let output = &sig.output;
+  let output = match output {
+    ReturnType::Default => panic!("Unexpected default return."),
+    ReturnType::Type(_arrow, t) => t
+  };
+  let block = &func.block;
+
+  let result = quote! {
+    #(#attrs)* #vis fn #name #new_generics(#arg1, #arg2, #arg3_i: &'r mut #arg3_t) -> 
+    std::pin::Pin<std::boxed::Box<dyn std::future::Future<Output = #output> + std::marker::Send + 'r>>
+    {
+      std::boxed::Box::pin( async move #block )
+    }
+  };
+  result.into()
+}
+
+#[proc_macro_attribute]
+pub fn native_tfn(_attr: TokenStream, item: TokenStream) -> TokenStream {
+  let func = parse_macro_input!(item as ItemFn);
+  let attrs = &func.attrs;
+  let vis = &func.vis;
+  let sig = &func.sig;
+  assert!(sig.asyncness.is_some());
+
+  let g0_lifetime = Lifetime::new("'r", Span::call_site());
+  let g0 = GenericParam::Lifetime(
+    LifetimeDef { attrs: Vec::new(), lifetime: g0_lifetime, colon_token: None, bounds: Punctuated::new() }
+  );
+  let mut generics = sig.generics.params.iter();
+  let g1 = generics.next();
+  let mut params = Punctuated::new();
+  params.insert(0, g0);
+  if let Some(g1) = g1 {
+    params.insert(1, g1.clone());
+  }
+  let new_generics = Generics {
+    lt_token: Some(Lt { spans: [Span::call_site()] }),
+    gt_token: Some(Gt { spans: [Span::call_site()] }),
+    params,
+    where_clause: None
+  };
+
+  let name = &sig.ident;
+  let mut inputs = sig.inputs.iter();
+  let arg1 = inputs.next().unwrap();
+  let arg3 = inputs.next().unwrap();
+  let (arg3_i, arg3_t) = match arg3 {
+    FnArg::Receiver(_) => panic!("Unexpected receiver argument."),
+    FnArg::Typed(t) => (&t.pat, &t.ty)
+  };
+  let arg3_t = match arg3_t.as_ref() {
+    Type::Reference(r) => {
+      assert!(r.mutability.is_none());
+      assert!(r.lifetime.is_none());
+      &r.elem
+    },
+    _ => panic!("Expected type reference in arg3")
+  };
+  let output = &sig.output;
+  let output = match output {
+    ReturnType::Default => panic!("Unexpected default return."),
+    ReturnType::Type(_arrow, t) => t
+  };
+  let block = &func.block;
+
+  let result = quote! {
+    #(#attrs)* #vis fn #name #new_generics (#arg1, #arg3_i: &'r #arg3_t) -> 
+    std::pin::Pin<std::boxed::Box<dyn std::future::Future<Output = #output> + std::marker::Send + 'r>>
+    {
+      std::boxed::Box::pin( async move #block )
+    }
+  };
+  result.into()
 }
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -1,7 +1,57 @@
-#[alchem_macros::native_fn]
-fn wrapped_function() {}
+//! Simple native_fn testing.
 
-#[test]
-fn works() {
-  wrapped_function();
+use alchem::collapsed::CollapsedInfo;
+use alchem::value::{Globals, MorphStatus, NativeInfo, NoCustom, Type, Value, CustomType};
+use alchem::vm::Runner;
+use alchem_macros::{native_fn, native_tfn};
+
+type Val = Value<NoCustom>;
+type Info = NativeInfo<NoCustom>;
+type CoInfo = CollapsedInfo<NoCustom>;
+type Run = Runner<NoCustom>;
+type Tp = Type<NoCustom>;
+type Gl = Globals<NoCustom>;
+type Status = MorphStatus<NoCustom>;
+
+#[native_tfn]
+async fn ntvt_number(_args: Vec<Tp>, _globals: &Option<Gl>) -> Status {
+  MorphStatus::NativeCompleted(Info::new(), Type::Number)
+}
+
+#[native_fn]
+async fn ntv_recall(_vals: Vec<Val>, _info: Option<CoInfo>, _runner: &mut Option<Run>) -> Val {
+  Val::String("hello".into())
+}
+
+#[native_tfn]
+async fn ntvt_eval<C: CustomType + 'static>(_args: Vec<Type<C>>, _globals: &Globals<C>) -> MorphStatus<C> {
+  let info = NativeInfo::new();
+  MorphStatus::NativeCompleted(info, Type::String(None))
+}
+
+#[native_fn]
+async fn ntv_eval<C: CustomType + 'static>(
+  _vals: Vec<Value<C>>, _info: Option<CollapsedInfo<C>>, _runner: &mut Option<Runner<C>>
+) -> Value<C> {
+  Value::String("fake".into())
+}
+
+#[tokio::test]
+async fn works_tfn() {
+  assert!(ntvt_number(Vec::new(), &None).await.is_known());
+}
+
+#[tokio::test]
+async fn works_fn() {
+  assert_eq!(ntv_recall(Vec::new(), None, &mut None).await.as_str(), "hello");
+}
+
+#[tokio::test]
+async fn works_tfn_c() {
+  assert!(ntvt_eval(Vec::new(), &Gl::new()).await.is_known());
+}
+
+#[tokio::test]
+async fn works_fn_c() {
+  assert_eq!(ntv_eval(vec![Val::Int(1)], None, &mut None).await.as_str(), "fake");
 }
